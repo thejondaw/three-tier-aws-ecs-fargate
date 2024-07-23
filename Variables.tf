0a082169ee3a6d23c9bf9975b52added56572e9e
thejondaw <mrjondaw@gmail.com>
2024-07-23 12:00:01 -0500

    Update

--- Variables.tf
+++ Variables.tf
@@ -1,30 +1,20 @@
 # ==================================================== #
-# ================= CIDR for Subnets ================= #
+
+# Variable for "AWS Provider" - Region:
+variable "region" {
+  description = "Please provide a region information"
+  type        = string
+  default     = ""
+}
+
 # ==================================================== #
 
-# # Variable for CIDR Block of Public Subnet #1:
-# variable "subnet_1_cidr" {
-#   description = "CIDR block for subnet 1 (WEB)"
-#   type        = string
-# }
-
-# # Variable for CIDR Block of Public Subnet #2:
-# variable "subnet_2_cidr" {
-#   description = "CIDR block for subnet 2 (ALB)"
-#   type        = string
-# }
-
-# # Variable for CIDR Block of Private Subnet #3:
-# variable "subnet_3_cidr" {
-#   description = "CIDR block for subnet 3 (API)"
-#   type        = string
-# }
-
-# # Variable for CIDR Block of Private Subnet #4:
-# variable "subnet_4_cidr" {
-#   description = "CIDR block for subnet 4 (DATABASE)"
-#   type        = string
-# }
+# Variable for "S3 Bucket" - Backend:
+variable "bucket" {
+  description = "Name of S3 bucket to store Terraform state"
+  type        = string
+  default     = ""
+}
 
 # ==================================================== #
 # ============= CIDR for VPC and Subnets ============= #
@@ -32,27 +22,32 @@
 
 # CIDR Block for "VPC":
 variable "vpc_cidr" {
-  default = "10.0.0.0/16"
+  description = "CIDR Block for VPC"
+  default     = "10.0.0.0/16"
 }
 
 # CIDR Block for "Public Subnet #1":
 variable "subnet_1_cidr" {
-  default = "10.0.1.0/24"
+  description = "CIDR Block for Subnet #1 (WEB)"
+  default     = "10.0.1.0/24"
 }
 
 # CIDR Block for "Public Subnet #2":
 variable "subnet_2_cidr" {
-  default = "10.0.2.0/24"
+  description = "CIDR Block for Subnet #2 (ALB)"
+  default     = "10.0.2.0/24"
 }
 
 # CIDR Block for "Private Subnet #3":
 variable "subnet_3_cidr" {
-  default = "192.168.3.0/24"
+  description = "CIDR Block for Subnet #3 (API)"
+  default     = "192.168.3.0/24"
 }
 
 # CIDR Block for "Private Subnet #4":
 variable "subnet_4_cidr" {
-  default = "192.168.4.0/24"
+  description = "CIDR Block for Subnet #4 (DATABASE)"
+  default     = "192.168.4.0/24"
 }
 
 # ==================================================== #

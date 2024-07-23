47dbc1c2de614df59f82293a42d50a00a220916b
thejondaw <mrjondaw@gmail.com>
2024-07-23 16:35:20 -0500

    Update

--- Modules/VPC/Provider.tf
+++ Modules/VPC/Provider.tf
@@ -1,17 +1,17 @@
 # ==================================================== #
 
-# # "AWS Provider" - Region:
-# provider "aws" {
-#   region = var.region
-# }
+# "AWS Provider" - Region:
+provider "aws" {
+  region = var.region
+}
 
-# # "S3 Bucket" - Backend for "VPC":
-# terraform {
-#   backend "s3" {
-#     bucket = var.bucket
-#     key    = "Toptal/VPC/terraform.tfstate"
-#     region = var.region
-#   }
-# }
+# "S3 Bucket" - Backend for "VPC":
+terraform {
+  backend "s3" {
+    bucket = var.bucket
+    key    = "Toptal/VPC/terraform.tfstate"
+    region = var.region
+  }
+}
 
 # ==================================================== #

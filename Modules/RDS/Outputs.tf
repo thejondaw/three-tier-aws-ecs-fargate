2487f78a8b61f6fc256b1f2f800797be00fddcba
thejondaw <mrjondaw@gmail.com>
2024-07-24 14:27:23 -0500

    Update

--- Modules/RDS/Outputs.tf
+++ Modules/RDS/Outputs.tf
@@ -1,8 +1,14 @@
 # ==================================================== #
 
+# # Output of Database Secret Manager:
+# output "secret_manager_db_arn" {
+#   value       = aws_secretsmanager_secret.secret_manager_db.arn
+#   description = "ARN of Secret Manager DB secret"
+# }
+
 # Output of Database Secret Manager:
 output "secret_manager_db_arn" {
-  value       = aws_secretsmanager_secret.secret_manager_db.arn
+  value       = aws_secretsmanager_secret.aurora_secret.arn
   description = "ARN of Secret Manager DB secret"
 }
 

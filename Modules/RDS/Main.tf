90036af5d0b29bab491049015fdbc1937d8949bf
thejondaw <mrjondaw@gmail.com>
2024-07-25 15:24:56 -0500

    Update

--- Modules/RDS/Main.tf
+++ Modules/RDS/Main.tf
@@ -34,7 +34,7 @@ resource "aws_rds_cluster_instance" "rds_instance" {
 # "Subnet Group" for Database:
 resource "aws_db_subnet_group" "aurora_subnet_group" {
   name       = "aurora-subnet-group"
-  subnet_ids = [var.subnet_api_cidr, var.subnet_db_cidr]
+  subnet_ids = [var.subnet_api_cidr.id, var.subnet_db_cidr.id]
 }
 
 # ==================================================== #

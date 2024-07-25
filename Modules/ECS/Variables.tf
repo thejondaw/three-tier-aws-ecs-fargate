22d2e4e2d39e3af00e2fd8b77b6d181b8c31b1ea
thejondaw <mrjondaw@gmail.com>
2024-07-25 15:09:17 -0500

    Update

--- Modules/ECS/Variables.tf
+++ Modules/ECS/Variables.tf
@@ -6,14 +6,12 @@
 variable "region" {
   description = "AWS Region"
   type        = string
-  default     = var.region
 }
 
 # Variable for "S3 Bucket":
 variable "bucket" {
   description = "Name of S3 bucket"
   type        = string
-  default     = var.bucket
 }
 
 # ==================================================== #
@@ -29,25 +27,21 @@ variable "secret_manager_db" {
 # Variable for CIDR Block of "VPC":
 variable "vpc_cidr" {
   description = "CIDR Block for VPC"
-  default     = var.vpc_cidr
 }
 
 # Variable for CIDR Block of "Public Subnet #1 (WEB)":
 variable "subnet_web_cidr" {
   description = "CIDR Block for Public Subnet #1 (WEB)"
-  default     = var.subnet_web_cidr
 }
 
 # Variable for CIDR Block of "Public Subnet #2 (ALB)":
 variable "subnet_alb_cidr" {
   description = "CIDR Block for Public Subnet #2 (ALB)"
-  default     = var.subnet_alb_cidr
 }
 
 # Variable for CIDR Block of "Private Subnet #3 (API)":
 variable "subnet_api_cidr" {
   description = "CIDR Block for Private Subnet #3 (API)"
-  default     = var.subnet_api_cidr
 }
 
 # ==================================================== #

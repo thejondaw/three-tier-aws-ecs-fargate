3beadf0a90f70c160defeab02ab135bf25ca616c
thejondaw <mrjondaw@gmail.com>
2024-07-25 16:07:38 -0500

    Update

--- Modules/RDS/Variables.tf
+++ Modules/RDS/Variables.tf
@@ -24,11 +24,13 @@ variable "vpc_cidr" {
 # Variable for CIDR Block of "Private Subnet #3 (API)":
 variable "subnet_api_cidr" {
   description = "CIDR Block for Private Subnet #3 (API)"
+  type = string
 }
 
 # Variable for CIDR Block of "Private Subnet #4 (DB)":
 variable "subnet_db_cidr" {
   description = "CIDR Block for Private Subnet #4 (DB)"
+  type = string
 }
 
 # ==================================================== #

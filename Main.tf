0a6cde507dcb69820acc1968e84217529dd7c820
thejondaw <mrjondaw@gmail.com>
2024-07-27 03:25:49 -0500

    Update

--- Main.tf
+++ Main.tf
@@ -11,24 +11,24 @@ provider "aws" {
 module "vpc" {
   source            = "./Modules/VPC"
   region            = var.region_rv
-  vpc_cidr          = var.vpc_cidr_rv
-  subnet_web_1_cidr = var.subnet_web_1_cidr_rv
-  subnet_web_2_cidr = var.subnet_web_2_cidr_rv
-  subnet_api_1_cidr = var.subnet_api_1_cidr_rv
-  subnet_api_2_cidr = var.subnet_api_2_cidr_rv
-  subnet_db_1_cidr  = var.subnet_db_1_cidr_rv
-  subnet_db_2_cidr  = var.subnet_db_2_cidr_rv
+  vpc_cidr          = var.vpc_cidr_id
+  subnet_web_1_cidr = var.subnet_web_1_cidr_id
+  subnet_web_2_cidr = var.subnet_web_2_cidr_id
+  subnet_api_1_cidr = var.subnet_api_1_cidr_id
+  subnet_api_2_cidr = var.subnet_api_2_cidr_id
+  subnet_db_1_cidr  = var.subnet_db_1_cidr_id
+  subnet_db_2_cidr  = var.subnet_db_2_cidr_id
 }
 
 # RDS Module
 module "rds" {
   source            = "./Modules/RDS"
   region            = var.region_rv
-  vpc_cidr          = module.vpc.vpc_rv
-  subnet_api_1_cidr = var.subnet_api_1_cidr_rv
-  subnet_api_2_cidr = var.subnet_api_2_cidr_rv
-  subnet_db_1_cidr  = var.subnet_db_1_cidr_rv
-  subnet_db_2_cidr  = var.subnet_db_2_cidr_rv
+  vpc_cidr          = module.vpc.vpc_id
+  subnet_api_1_cidr = module.vpc.subnet_api_1_cidr_id
+  subnet_api_2_cidr = module.vpc.subnet_api_2_cidr_id
+  subnet_db_1_cidr  = module.vpc.subnet_db_1_cidr_id
+  subnet_db_2_cidr  = module.vpc.subnet_db_2_cidr_id
 }
 
 # ECS Module
@@ -36,12 +36,12 @@ module "ecs" {
   source            = "./Modules/ECS"
   region            = var.region_rv
   vpc_cidr          = module.vpc.vpc_id
-  subnet_web_1_cidr = var.subnet_web_1_cidr_rv
-  subnet_web_2_cidr = var.subnet_web_2_cidr_rv
-  subnet_api_1_cidr = var.subnet_api_1_cidr_rv
-  subnet_api_2_cidr = var.subnet_api_2_cidr_rv
-  subnet_db_1_cidr  = var.subnet_db_1_cidr_rv
-  subnet_db_2_cidr  = var.subnet_db_2_cidr_rv
+  subnet_web_1_cidr = module.vpc.subnet_web_1_cidr_id
+  subnet_web_2_cidr = module.vpc.subnet_web_2_cidr_id
+  subnet_api_1_cidr = module.vpc.subnet_api_1_cidr_id
+  subnet_api_2_cidr = module.vpc.subnet_api_2_cidr_id
+  subnet_db_1_cidr  = module.vpc.subnet_db_1_cidr_id
+  subnet_db_2_cidr  = module.vpc.subnet_db_2_cidr_id
 }
 
 # ==================================================== #
--- Variables.tf
+++ Variables.tf
@@ -9,43 +9,43 @@ variable "region_rv" {
 # ============= CIDR for VPC and Subnets ============= #
 
 # Variable for CIDR Block of VPC
-variable "vpc_cidr_rv" {
+variable "vpc_cidr_id" {
   description = "CIDR Block for VPC"
   type        = string
 }
 
 # Variable for CIDR Block of WEB Subnet #1 - Public
-variable "subnet_web_1_cidr_rv" {
+variable "subnet_web_1_cidr_id" {
   description = "CIDR Block for WEB Subnet #1 - Public"
   type        = string
 }
 
 # Variable for CIDR Block of WEB Subnet #2 - Public
-variable "subnet_web_2_cidr_rv" {
+variable "subnet_web_2_cidr_id" {
   description = "CIDR Block for WEB Subnet #2 - Public"
   type        = string
 }
 
 # Variable for CIDR Block of API Subnet #1 - Private
-variable "subnet_api_1_cidr_rv" {
+variable "subnet_api_1_cidr_id" {
   description = "CIDR Block for API Subnet #1 - Private"
   type        = string
 }
 
 # Variable for CIDR Block of API Subnet #2 - Private
-variable "subnet_api_2_cidr_rv" {
+variable "subnet_api_2_cidr_id" {
   description = "CIDR Block for API Subnet #2 - Private"
   type        = string
 }
 
 # Variable for CIDR Block of DB Subnet #1 - Private
-variable "subnet_db_1_cidr_rv" {
+variable "subnet_db_1_cidr_id" {
   description = "CIDR Block for DB Subnet #1 - Private"
   type        = string
 }
 
 # Variable for CIDR Block of DB Subnet #2 - Private
-variable "subnet_db_2_cidr_rv" {
+variable "subnet_db_2_cidr_id" {
   description = "CIDR Block for DB Subnet #2 - Private"
   type        = string
 }

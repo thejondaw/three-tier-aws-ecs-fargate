7eedd7c40c97e3f8207c334560501354c1639301
thejondaw <mrjondaw@gmail.com>
2024-07-28 20:23:49 -0500

    Update

--- Modules/ECS/Main.tf
+++ Modules/ECS/Main.tf
@@ -374,27 +374,27 @@ resource "aws_appautoscaling_policy" "web_cpu" {
 
 # ----------- TEST -----------------
 
-# resource "aws_service_discovery_private_dns_namespace" "main" {
-#   name        = "myapp.local"
-#   description = "Private DNS namespace for my app"
-#   vpc         = data.aws_vpc.main.id
-# }
+resource "aws_service_discovery_private_dns_namespace" "main" {
+  name        = "myapp.local"
+  description = "Private DNS namespace for my app"
+  vpc         = data.aws_vpc.main.id
+}
 
-# resource "aws_service_discovery_service" "api" {
-#   name = "api"
+resource "aws_service_discovery_service" "api" {
+  name = "api"
 
-#   dns_config {
-#     namespace_id = aws_service_discovery_private_dns_namespace.main.id
+  dns_config {
+    namespace_id = aws_service_discovery_private_dns_namespace.main.id
 
-#     dns_records {
-#       ttl  = 10
-#       type = "A"
-#     }
+    dns_records {
+      ttl  = 10
+      type = "A"
+    }
 
-#     routing_policy = "MULTIVALUE"
-#   }
+    routing_policy = "MULTIVALUE"
+  }
 
-#   health_check_custom_config {
-#     failure_threshold = 1
-#   }
-# }
+  health_check_custom_config {
+    failure_threshold = 1
+  }
+}

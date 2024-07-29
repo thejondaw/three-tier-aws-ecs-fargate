1be56e34710079f102e1f020b64a326a1215a135
thejondaw <mrjondaw@gmail.com>
2024-07-28 21:04:58 -0500

    Update

--- Modules/ECS/Main.tf
+++ Modules/ECS/Main.tf
@@ -54,12 +54,12 @@ resource "aws_security_group" "ecs_tasks" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # ingress {
-  #   from_port   = 0
-  #   to_port     = 0
-  #   protocol    = "-1"
-  #   self        = true
-  # }
+  egress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = [var.vpc_cidr]
+  }
 
   egress {
     from_port   = 0

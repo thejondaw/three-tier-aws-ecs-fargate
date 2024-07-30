3f656e804ab0c4d7a20bd3e7dc85ebaaae02a0be
thejondaw <mrjondaw@gmail.com>
2024-07-30 17:35:59 -0500

    Update

--- Modules/ECS/Main.tf
+++ Modules/ECS/Main.tf
@@ -208,7 +208,7 @@ resource "aws_lb_listener_rule" "api" {
 # "Target Group" for "WEB" Application
 resource "aws_lb_target_group" "web" {
   name        = "web-tg"
-  port        = 4000
+  port        = 80
   protocol    = "HTTP"
   vpc_id      = data.aws_vpc.main.id
   target_type = "ip"

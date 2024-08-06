8f5610ff16689dbc0bd7c75d78585978fd8650d5
thejondaw <mrjondaw@gmail.com>
2024-08-06 17:34:31 -0500

    Update

--- Modules/ECS/Main.tf
+++ Modules/ECS/Main.tf
@@ -312,7 +312,8 @@ resource "aws_ecs_task_definition" "web" {
       environment = [
         {
           name  = "API_HOST"
-          value = "http://${aws_lb.main.dns_name}:3000"
+          #value = "http://${aws_lb.main.dns_name}:3000"
+          value = "http://api.internal.local:3000" #! TEST
         }
       ]
       logConfiguration = {
@@ -336,6 +337,10 @@ resource "aws_ecs_service" "api" {
   task_definition = aws_ecs_task_definition.api.arn
   desired_count   = 1
   launch_type     = "FARGATE"
+
+  service_registries {                                    #! TEST
+    registry_arn = aws_service_discovery_service.api.arn  #! TEST
+  }                                                       #! TEST
 
   network_configuration {
     subnets          = [data.aws_subnet.api_1.id, data.aws_subnet.api_2.id]
@@ -442,3 +447,28 @@ resource "aws_cloudwatch_log_group" "web" {
 }
 
 # ==================================================== #
+
+resource "aws_service_discovery_private_dns_namespace" "namespace" {
+  name        = "internal.local"
+  description = "Private DNS namespace for internal services"
+  vpc         = data.aws_vpc.main.id
+}
+
+resource "aws_service_discovery_service" "api" {
+  name = "api"
+
+  dns_config {
+    namespace_id = aws_service_discovery_private_dns_namespace.namespace.id
+
+    dns_records {
+      ttl  = 10
+      type = "A"
+    }
+
+    routing_policy = "MULTIVALUE"
+  }
+
+  health_check_custom_config {
+    failure_threshold = 1
+  }
+}
